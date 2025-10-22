ea770200a3faaceb8262eaec7471b0f0cd3157ba
Cy <ipskl168@gmail.com>
2025-10-22 15:40:34 +0800

    Update constants.js

--- app/constant/constants.js
+++ app/constant/constants.js
@@ -1,4 +1,5 @@
 export const apiUrl = "https://icom.ipsgroup.com.my/api/";
 export const customerUrl = "https://icom.ipsgroup.com.my/"; //for login
-export const imageUrl = "https://uspizza-uploads.s3.ap-southeast-5.amazonaws.com/"
+// export const imageUrl = "https://uspizza-uploads.s3.ap-southeast-5.amazonaws.com/" //live
+export const imageUrl = "https://icom.ipsgroup.com.my/backend/uploads/" //dev
 // export const apiUrl = "https://pos.ipsgroup.com.my/api/";
